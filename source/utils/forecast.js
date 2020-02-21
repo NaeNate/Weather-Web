90dc19ee06a606898b8013463bc28470105aad62
NaeNate <nate@qstreet.org>
2020-02-20 19:50:38 -0500

    E

--- source/utils/forecast.js
+++ source/utils/forecast.js
@@ -3,17 +3,17 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'https://api.darksky.net/forecast/6a4f86c199afab05342e0438006efab0/' + longitude + ',' + latitude + '?lang=en&units=us'
 
-  request({ url, json: true}, (error, response) => {
-  if (error) {
-  callback('Unable to connect to weather services', undefined)
-  } else if (response.body.error) {
-    callback('Unable to find loaction', undefined)
-  } else (
-    callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees. There is a ' + response.body.currently.precipProbability * 100 + '% chance of rain. The wind speed is currently ' + response.body.currently.windSpeed + ' miles per hour.'
+  request({ url, json: true }, (error, response) => {
+    if (error) {
+      callback('Unable to connect to weather services', undefined)
+    } else if (response.body.error) {
+      callback('Unable to find loaction', undefined)
+    } else (
+      callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees. There is a ' + response.body.currently.precipProbability * 100 + '% chance of rain. The wind speed is currently ' + response.body.currently.windSpeed + ' miles per hour.'
+      )
     )
-  )
   })
-  
+
 }
 
 module.exports = forecast
